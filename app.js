b306623c8bca14c16c21e61baa70b61542350ff8
box <49935142@qq.com>
2017-10-23 17:46:14 +0800

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by box on 2017/10/23.
+ */
+var express = require('express');
+var app = express();
+
+var config = require('./config.json');
+var listenPort = config.port;
+
+process.on('uncaughtException', function (err) {
+    console.error('Caught exception: ' + err.stack);
+});
+
+// 上传配置
+app.get('/upload_config', function (req, res) {
+    // 保存到数据库
+    var result = {msg:"success"};
+    res.send(result);
+});
+
+app.listen(listenPort);
+console.log("listen port", listenPort);
